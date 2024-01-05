c27bd65c1505799ff631852154af2d34e420c4e0
mico <mico.viola@myecloud.net>
2024-01-05 18:00:24 +0800

    provision dev

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "oss" {
-    bucket = "dynamic_env-bokbok-bucket"
+    bucket = "dynamic_env-bok-bucket"
     key   = "dynamic_env-state.tfstate"
     region = "ap-southeast-6"
   }

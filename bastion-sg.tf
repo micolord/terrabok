324b0121af3b93d87b9d873b1aa70487ca62cc6a
Paolo <peraltadavidpaolo@gmail.com>
2024-01-15 02:00:23 +0800

    added bastion sg and public ip

--- bastion-sg.tf
+++ bastion-sg.tf
@@ -1,6 +1,7 @@
 resource "alicloud_security_group" "bastion-sg" {
-  name        = "${var.env_name}-${var.project}-fe-sg"
+  name        = "${var.env_name}-${var.project}-bastion-sg"
   description = "${var.env_name}-${var.project} security group"
+  security_group_type = "enterprise"
   vpc_id = module.vpc.vpc_id
 }
 

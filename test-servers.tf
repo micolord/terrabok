4eab89385ddc6bf049fd9a1d483a93fddd035afd
Paolo <peraltadavidpaolo@gmail.com>
2024-01-18 23:29:11 +0800

    addded test servers

--- test-servers.tf
+++ test-servers.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "redis" {
+  name     = "redis-resources"
+  location = "West Europe"
+}
+
 resource "alicloud_instance" "bo_ecs_instance_test_1" {
   instance_name        = "${var.env_name}-${var.project}-testing-fe"
   image_id             = "ubuntu_20_04_x64_20G_alibase_20231221.vhd"
@@ -29,7 +34,7 @@ resource "alicloud_instance" "bo_ecs_instance_test_2" {
 
 resource "azurerm_redis_cache" "redis_instance" {
   name                = "${var.env_name}-${var.project}-testing-cache"
-  location            = azurerm_resource_group.example.location
+  location            = "Default Resource Group"
   capacity            = 2
   family              = "C"
   sku_name            = "Standard"

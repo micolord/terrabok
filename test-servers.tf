a2cf0544ba727e9eedf7c6fb674db537f7a6dd19
Paolo <peraltadavidpaolo@gmail.com>
2024-01-18 23:30:23 +0800

    addded test servers

--- test-servers.tf
+++ test-servers.tf
@@ -35,6 +35,8 @@ resource "alicloud_instance" "bo_ecs_instance_test_2" {
 resource "azurerm_redis_cache" "redis_instance" {
   name                = "${var.env_name}-${var.project}-testing-cache"
   location            = "Default Resource Group"
+  location            = "Manila"
+  resource_group_name = "Default Resource Group"
   capacity            = 2
   family              = "C"
   sku_name            = "Standard"

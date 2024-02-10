aa408e22954bb1b63201aa0c0354db6047a5f57e
mico <michaelangeloviola@gmauil.com>
2024-02-10 23:34:54 +0800

    added ssl

--- variables.tf
+++ variables.tf
@@ -38,30 +38,30 @@ variable "cert_id" {
     description = "Cert ID"
 }
 
-#variable "gl_fe_domain" {
-#    description = "GL FE Domain"
-#}
+variable "gl_fe_domain" {
+    description = "GL FE Domain"
+}
 
-#variable "gl_be_domain" {
-#    description = "GL BE Domain"
-#}
+variable "gl_be_domain" {
+    description = "GL BE Domain"
+}
 
-#variable "bo_fe_domain" {
-#    description = "BO FE Domain"
-#}
+variable "bo_fe_domain" {
+    description = "BO FE Domain"
+}
 
-#variable "bo_be_domain" {
-#    description = "BO BE Domain"
-#}
+variable "bo_be_domain" {
+    description = "BO BE Domain"
+}
 
-#variable "jobproc_domain" {
-#    description = "Jobproc Domain"
-#}
+variable "jobproc_domain" {
+    description = "Jobproc Domain"
+}
 
-#variable "socket_domain" {
-#    description = "Socket Domain"
-#}
+variable "socket_domain" {
+    description = "Socket Domain"
+}
 
-#variable "cert_id" {
-#    description = "Cert ID"
-#}
+variable "cert_id" {
+    description = "Cert ID"
+}
